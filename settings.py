8e67b32ff1439ac427ad735df6fb289cf24bb4f4
Zhang Chuan <zhang.chuan@ymail.com>
2012-10-12 14:12:33 +0800

    Zhang Chuan

--- settings.py
+++ settings.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #coding=utf-8
 
 from flask import Flask
@@ -7,9 +6,14 @@ from string import ascii_uppercase
 
 app = Flask(__name__)
 app.config['MONGOALCHEMY_SAFE_SESSION'] = False
-app.config['MONGOALCHEMY_SERVER'] = 'localhost'
+app.config['DEBUG'] = True
+#app.config['MONGOALCHEMY_SERVER'] = '118.122.117.134'
+app.config['MONGOALCHEMY_SERVER'] = '216.12.198.98'
 app.config['MONGOALCHEMY_DATABASE'] = 'opscm'
 app.config['MONGOALCHEMY_PORT'] = 27017
-app.config['DEBUG'] = True
+app.config['MONGOALCHEMY_USER'] = 'opscm'
+app.config['MONGOALCHEMY_PASSWORD'] = 'stm123'
+app.config['MONGOALCHEMY_SERVER_AUTH'] = False
+
 db = MongoAlchemy(app)
 db.init_app(app)
