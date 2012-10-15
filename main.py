52ed2ae3b26e093b546b0d77ab57dab3de18224a
Zhang Chuan <zhang.chuan@ymail.com>
2012-10-16 00:47:00 +0800

    Zhang Chuan

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from settings import app
 from documents import syslog
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from datetime import datetime, timedelta
 
-today = datetime.strptime(datetime.now().strftime('%m/%d/%Y'), '%m/%d/%Y')
+today = datetime.strptime(datetime.now() \
+                .strftime('%m/%d/%Y'), '%m/%d/%Y')
 tomorrow = today + timedelta(days=1)
 
 
@@ -12,44 +13,43 @@ tomorrow = today + timedelta(days=1)
 def list_syslog(category, page=1):
     title = u'OPeration System Log'
     syslog.config_collection_name = 'syslog.' + category
-    pagination = syslog.query.regexp(
-                            bdate=today,
-                            edate=tomorrow).descending('time').paginate(
-                                                               page=page,
-                                                               per_page=20)
+    pagination = syslog.query.regexp(bdate=today, edate=tomorrow) \
+                                     .descending('time') \
+                                     .paginate(page=page, per_page=50)
     return render_template('/log/list_syslog.html',
                             category=category,
                             pagination=pagination,
                             title=title)
 
-@app.route('/log/<category>/filter/', methods=['GET'])
-@app.route('/log/<category>/filter/<int:page>')
+
+@app.route('/log/<category>/filter/', methods=['POST', 'GET'])
+@app.route('/log/<category>/filter/<int:page>', methods=['POST', 'GET'])
 def filter_syslog(category, page=1):
     title = u'OPeration System Log'
     syslog.config_collection_name = 'syslog.' + category
-    re_host = request.args.get('hostname','')
-    re_message = request.args.get('letter','')
-
     try:
-        startdate = datetime.strptime(
-                    request.args.get('daterange','').split('-')[0].strip(),
-                    '%m/%d/%Y')
-        enddate = datetime.strptime(
-                    request.args.get('daterange','').split('-')[1].strip(),
-                    '%m/%d/%Y')
+        session['hostname'] = request.form['hostname']
+        session['keyword'] = request.form['keyword']
+        startdate = datetime.strptime(request.form['daterange'] \
+                                                   .split('-')[0].strip(),
+                                                   '%m/%d/%Y')
+        enddate = datetime.strptime(request.form['daterange'] \
+                                                 .split('-')[1].strip(),
+                                                 '%m/%d/%Y')
     except:
         startdate = today
         enddate = tomorrow
-    pagination = syslog.query.regexp(
-        host=re_host,
-        message=re_message,
-        bdate=startdate,
-        edate=enddate).descending(
-            'time').paginate(page=page, per_page=20)
+
+    pagination = syslog.query.regexp(host = session['hostname'],
+                                     message = session['keyword'],
+                                     bdate = startdate,
+                                     edate = enddate) \
+                                     .descending('time') \
+                                     .paginate(page = page, per_page = 50)
+    
     return render_template('/log/filter_syslog.html',
-                           category=category,
-                           pagination=pagination,
-                           title=title)
+                            category=category,
+                            pagination=pagination,title=title)
 
 
 @app.route('/')
